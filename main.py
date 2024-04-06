f6b9038e5b4331e83a53cc0658bbadbfbd150376
Andrew Bright <legendarkakr@gmail.com>
2024-04-06 20:53:45 +0300

    router flights added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import asyncio, logging, sys
 from aiogram import Bot, Dispatcher
-from handlers import questions, visit, admins, chat, personal_questions, wine_questions
+from handlers import questions, visit, admins, chat, personal_questions, wine_questions, flight
 from forwarding import forward
 
 TG_TOKEN =  '6440298772:AAGL48-IZCl5D2Lxcn_VHAfAfKnk8GN3_hI' #Laura GPT
@@ -12,7 +12,7 @@ dp = Dispatcher()
 
 
 async def main() -> None:
-    dp.include_routers(admins.router, questions.router, visit.router, chat.router, forward.router, personal_questions.router, wine_questions.router)
+    dp.include_routers(admins.router, questions.router, visit.router, chat.router, forward.router, personal_questions.router, wine_questions.router, flight.router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
