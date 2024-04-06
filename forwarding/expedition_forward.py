075747b14b284c1a17ff7f82e693083076f84b9c
Andrew Bright <legendarkakr@gmail.com>
2024-04-06 22:38:24 +0300

    mid  added

--- forwarding/expedition_forward.py
+++ forwarding/expedition_forward.py
@@ -19,18 +19,18 @@ async def start_msg(message: Message, command: CommandObject):
     await message.bot.delete_message(chat_id=message.chat.id,message_id=message.message_id)
     albm = []
     post_id = int(command.args)
-    for i in range(10):
-            try:  
-                msg = await message.bot.forward_message(chat_id=1263494893, from_chat_id=from_chanal_id, message_id=post_id)
-                if (msg.caption != None) & (albm != []):
-                    await message.bot.copy_messages(chat_id=message.chat.id, message_thread_id=message.message_thread_id, from_chat_id=from_chanal_id, message_ids=albm)
-                    albm = []
-                    break
-                else: 
-                    albm.append(post_id)      
-                    post_id=post_id+1        
-            except Exception as E:
-                print(E)
-                post_id=post_id+1                
+    for i in range(15):
+        try:  
+            msg = await message.bot.forward_message(chat_id=1263494893, from_chat_id=from_chanal_id, message_id=post_id)
+            if (msg.caption != None) & (albm != []):
+                await message.bot.copy_messages(chat_id=message.chat.id, message_thread_id=message.message_thread_id, from_chat_id=from_chanal_id, message_ids=albm)
+                albm = []
+                break
+            else: 
+                albm.append(post_id)      
+                post_id=post_id+1        
+        except Exception as E:
+            print(E)
+            post_id=post_id+1                
                 
 
