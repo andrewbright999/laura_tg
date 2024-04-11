f64cbfeabc4175276cb08bd34b507ef676e94e08
Andrew Bright <legendarkakr@gmail.com>
2024-04-11 22:54:39 +0300

    codec

--- handlers/admins.py
+++ handlers/admins.py
@@ -50,7 +50,7 @@ async def laura_copy_message(message: Message):
         file_id = message.video.file_id
         file = await message.bot.get_file(file_id)
         await message.bot.download_file(file.file_path, "video.mov")
-        clip = VideoFileClip("video.mov",codec="libx264")
+        clip = VideoFileClip("video.mov")
         clip_resized = clip.resize(width=299)
         cropped_clip = crop(clip_resized, x1 = 1, y1 = 1,  width = 399, height = 399)
         clip_resized = cropped_clip.resize(width=299)
