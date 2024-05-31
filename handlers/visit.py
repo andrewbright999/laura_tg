79580d648507c3ca9539303895cd249148fd51b3
Andrew Bright <legendarkakr@gmail.com>
2024-05-31 13:46:10 +0300

    leave messages delete

--- handlers/visit.py
+++ handlers/visit.py
@@ -1,5 +1,7 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from aiogram.filters import ChatMemberUpdatedFilter, IS_NOT_MEMBER
+from aiogram.types import ChatMemberUpdated
 
 router = Router() 
 router.message.filter(lambda message: message.from_user.is_bot == False)
@@ -22,6 +24,9 @@ async def somebody_added(message: Message):
                            
   Royal Fishing Club, здесь сбываются рыболовные мечты!""", parse_mode="HTML")
 
-@router.message(F.old_chat_members)
-async def somebody_leave(message: Message):
-    await message.delete()        
+@router.chat_member.update(ChatMemberUpdatedFilter(IS_NOT_MEMBER))
+async def handle_member_leave(event: ChatMemberUpdated):
+    # Получаем сообщение, которое нужно удалить
+    message = event.message
+    if message:
+        await message.bot.delete_message(message.chat.id, message.message_id)
