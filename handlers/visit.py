aa5109eec851381445194ac0d5960d36f8b4130f
Andrew Bright <legendarkakr@gmail.com>
2024-06-26 16:09:46 +0300

    chat updates deletes

--- handlers/visit.py
+++ handlers/visit.py
@@ -1,4 +1,4 @@
-from aiogram import Router, F
+from aiogram import Router, F, types
 from aiogram.types import Message
 from aiogram.filters import ChatMemberUpdatedFilter, IS_NOT_MEMBER
 from aiogram.types import ChatMemberUpdated
@@ -6,13 +6,9 @@ from aiogram.types import ChatMemberUpdated
 router = Router() 
 router.message.filter(lambda message: message.from_user.is_bot == False)
 
-@router.message(F.new_chat_members)
-async def somebody_added(message: Message):
-    await message.delete()
-
-@router.chat_member.update(ChatMemberUpdatedFilter(IS_NOT_MEMBER))
-async def handle_member_leave(event: ChatMemberUpdated):
-    # Получаем сообщение, которое нужно удалить
-    message = event.message
-    if message:
-        await message.bot.delete_message(message.chat.id, message.message_id)
+@router.chat_member()
+async def on_chat_member_updated(event: ChatMemberUpdated):
+    if event.new_chat_member.status in [types.ChatMemberStatus.LEFT, types.ChatMemberStatus.KICKED]:
+        await event.bot.delete_message(chat_id=event.chat.id, message_id=event.from_user.id)
+    elif event.new_chat_member.status == types.ChatMemberStatus.MEMBER:
+        await event.bot.delete_message(chat_id=event.chat.id, message_id=event.from_user.id)
